c14913faf5a6e412cc65ee2b476e46ed5512e223
Jason Lee <huacnlee@gmail.com>
2011-11-18 14:15:12 +0800

    -

--- templates/rails/scaffold_controller/controller.rb
+++ templates/rails/scaffold_controller/controller.rb
@@ -1,10 +1,8 @@
 # coding: UTF-8
-class <%= controller_class_name %>Controller < ApplicationController
+class <%= controller_class_name %>Controller < <%= controller_class_name.include?('::') == true ? "#{controller_class_name.split('::').first}::" : ''  %>ApplicationController
   
   def index
-    @<%= plural_file_name %> = initialize_grid(<%= file_name.camelize %>, 
-      :order => 'id',
-      :order_direction => 'desc')
+    @<%= plural_file_name %> = <%= file_name.camelize %>.recent.paginate(:page => params[:page], :per_page => 20)
 
     respond_to do |format|
       format.html # index.html.erb
